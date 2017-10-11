24d76d5b0cd849e8c1bb6b6360409657f3bcb5e4
Sagar J Aivale <saivale@localhost.localdomain>
2017-10-11 23:56:46 +0530

    Accelerometer ADXL interface to STM32F407

---
+++ FINAL_SPI_ADXL/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'FINAL_SPI_ADXL' 
+ * Target:  'FINAL_SPI_ADXL' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
